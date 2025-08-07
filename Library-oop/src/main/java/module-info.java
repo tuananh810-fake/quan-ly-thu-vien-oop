e885670c560adac5536e57c596baa4d88eae7712
tuananh810-fake <anh2006t@gmail.com>
2025-08-07 10:02:06 +0700

    Khuôn dự án

--- Library-oop/src/main/java/module-info.java
+++ Library-oop/src/main/java/module-info.java
@@ -1,8 +1,13 @@
-module org.example.libraryoop {
+module com.example.libary {
+    // Khai báo rằng module này cần dùng thư viện javafx.controls
     requires javafx.controls;
+
+    // Khai báo rằng module này cần dùng thư viện javafx.fxml
     requires javafx.fxml;
 
+    // Mở package của bạn cho thư viện javafx.fxml để nó có thể truy cập vào HelloController
+    opens com.example.libary to javafx.fxml;
 
-    opens org.example.libraryoop to javafx.fxml;
-    exports org.example.libraryoop;
+    // Xuất khẩu (exports) package chính để JavaFX có thể khởi chạy ứng dụng
+    exports com.example.libary;
 }
---
+++ src/main/java/main/java/org/example/libraryoop/infrastructure/SQLLoansRepository.java
@@ -0,0 +1,5 @@
+package main.java.org.example.libraryoop.infrastructure;
+
+public class SQLLoansRepository {
+    
+}
